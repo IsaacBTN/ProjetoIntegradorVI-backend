739b20eff348ca2d4c778c12ecab66660a73c3e4
IsaacBTN <isaacbertonha3@gmail.com>
2024-09-01 20:51:24 -0300

    Adição dos services para user, consumption e food

--- src/main/java/com/projetoIntegrador/Projeto_Integrador_backend/entities/User.java
+++ src/main/java/com/projetoIntegrador/Projeto_Integrador_backend/entities/User.java
@@ -12,12 +12,81 @@ public class User {
    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    private String sexo;
+    private int idade;
     @Column(unique = true)
     private String email;
     private String password;
 
+    private Double altura;
+    private Double peso;
+
+    private Double gastoBasal;
     @OneToMany(mappedBy = "User")
     private List<Consumption> consumptions;
+
+    public Double getGastoBasal() {
+        return gastoBasal;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setGastoBasal(Double gastoBasal) {
+        this.gastoBasal = gastoBasal;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+
+    public double calcBasal(double peso, double altura, int idade, String sexo) {
+        double gastoBasal;
+
+        if (sexo.equalsIgnoreCase("masculino")) {
+            gastoBasal = (10 * peso) + (6.25 * altura) - (5 * idade) + 5;
+        } else if (sexo.equalsIgnoreCase("feminino")) {
+            gastoBasal = (10 * peso) + (6.25 * altura) - (5 * idade) - 161;
+        } else {
+            throw new IllegalArgumentException("Sexo deve ser 'masculino' ou 'feminino'");
+        }
+
+        return gastoBasal;
+    }
+
+    public Double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(Double altura) {
+        this.altura = altura;
+    }
+
+    public Double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(Double peso) {
+        this.peso = peso;
+    }
+
+    public List<Consumption> getConsumptions() {
+        return consumptions;
+    }
+
+    public void setConsumptions(List<Consumption> consumptions) {
+        this.consumptions = consumptions;
+    }
 
 
     public Long getId() {
@@ -55,11 +124,16 @@ public class User {
     public User() {
     }
 
-
-    public User(Long id, String name, String email, String password) {
+    public User(Long id, String name, String sexo, int idade, String email, String password, Double altura, Double peso, Double gastoBasal, List<Consumption> consumptions) {
         this.id = id;
         this.name = name;
+        this.sexo = sexo;
+        this.idade = idade;
         this.email = email;
         this.password = password;
+        this.altura = altura;
+        this.peso = peso;
+        this.gastoBasal = gastoBasal;
+        this.consumptions = consumptions;
     }
 }
